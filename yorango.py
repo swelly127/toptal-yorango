77d71abd27aa4a5d13cbc513790483ef3730d67d
jessica <jessica@digit.co>
2019-04-21 21:40:01 -0700

    Implement listing filters

--- yorango.py
+++ yorango.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from models import *
-from flask_bcrypt import Bcrypt
-from flask_wtf.csrf import CSRFProtect
 import logging
 import os
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+from geopy.geocoders import Nominatim
+from models import *
 
 mongo_host = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017')
 connect(alias='default', host=mongo_host)
@@ -13,6 +15,10 @@ app.config.from_object('settings')
 bcrypt = Bcrypt(app)
 csrf = CSRFProtect(app)
 csrf.init_app(app)
+
+geolocator = Nominatim(user_agent="yorango")
+
+VERY_LARGE_INT = 100000000
 
 @app.route('/')
 def index():
@@ -24,18 +30,38 @@ def index():
 @app.route('/listings', methods=['GET', 'POST'])
 def listings():
     if request.method == 'POST':
-        # Create a new listing
+        title = request.form.get('name')
+        description = request.form.get('description', '')
+        sq_ft = request.form['sq_ft']
+        num_rooms = request.form['num_rooms']
+        monthly_rent = request.form['monthly_rent']
+        address = request.form['address']
+        location = geolocator.geocode(address)
+        latitude = location.latitude
+        longitude = location.longitude
+        is_available = request.form.get('is_available') == "true"
+        new_user = Listing(
+            title=title,
+            description=description,
+            sq_ft=sq_ft,
+            num_rooms=num_rooms,
+            monthly_rent=monthly_rent,
+            address=address,
+            is_available=is_available,
+            coordinates=[longitude, latitude],
+        )
+        new_user.save()
         return redirect(url_for('listings'))
     sort_order = request.args.get('sort', 'list')
-    price_low = request.form.get('price_low')
-    price_high = request.form.get('price_high')
-    size_max = request.form.get('size_max')
-    size_min = request.form.get('size_min')
-    num_rooms_max = request.form.get('num_rooms_max')
-    num_rooms_min = request.form.get('num_rooms_min')
+    price_low = request.form.get('price_low', 0)
+    price_high = request.form.get('price_high', VERY_LARGE_INT) # Equal to $100M rent
+    size_max = request.form.get('size_max', VERY_LARGE_INT) # Equal to 2300 acres
+    size_min = request.form.get('size_min', 0)
+    num_rooms_max = request.form.get('num_rooms_max', VERY_LARGE_INT)
+    num_rooms_min = request.form.get('num_rooms_min', 0)
     listings = Listing.objects(
-        monthly_rent__lte=price_high, 
-        monthly_rent__gte=price_low, 
+        monthly_rent__lte=price_high,
+        monthly_rent__gte=price_low,
         sq_ft__lte=size_max,
         sq_ft__gte=size_min,
         num_rooms__lte=num_rooms_max,
@@ -64,6 +90,7 @@ def single_listing(listing_id):
 
 @app.route('/users')
 def users():
+    # todo(jshu): only show this page if the logged in user is admin
     users = User.objects.all()
     return render_template('users.html', users=users)
 
